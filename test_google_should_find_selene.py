69f26fffa087a8439548b415e7c723382d1167c7
ElenaRog <q.www.ta@gmail.com>
2023-07-12 21:39:32 +0300

    1. Добавлена фикстура измнения размера окна браузера 2. Добавлена функция для теста с именем test_positive_search 3. Добавлен тест на поиск без результатов

--- test_google_should_find_selene.py
+++ test_google_should_find_selene.py
@@ -1,8 +1,21 @@
+import pytest
 from selene.support.shared import browser
 from selene import be, have
 
 
-def test_google_search():
+@pytest.fixture(autouse=True)
+def set_browser_size():
+    browser.config.window_width = 480
+    browser.config.window_height = 720
+
+
+def test_positive_search():
     browser.open('https://google.com')
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+
+
+def test_negative_search():
+    browser.open('https://google.com')
+    browser.element('[name="q"]').should(be.blank).type('OfhhriUJHFsgr547udSYGDqwtsg94jJcnefsakZbdfkjlUH').press_enter()
+    browser.element('[id="result-stats"]').should(have.text('Результатов: примерно 0'))
